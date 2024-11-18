b9148ac181a2010584382a9b897279fab4e46133
Renaldo Louis <renaldo.louis@commsult.id>
2024-11-18 18:38:11 +0700

    first commit

---
+++ src/main/java/com/library/app/ws/LibraryAppWsApplication.java
@@ -0,0 +1,13 @@
+package com.library.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppWsApplication.class, args);
+	}
+
+}
